81e60b4c1831dec4e8755a1e279a6b30b34798c8
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-25 20:36:36 +0100

    [fix] script

--- script.js
+++ script.js
@@ -11,7 +11,32 @@ const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
 axios.get(endpoint)
 .then(responseObj => {
     // codice da eseguire in caso di successo
-    console.log(responseObj);
+    const pictures = responseObj.data;
+    // console.log(pictures);
+
+    for(let i = 0; i < pictures.length; i++){
+        let picture = pictures[i];
+        console.log(picture);
+        // Destrutturo l'oggetto
+        const {title, date, url} = picture;
+
+        // Creo le card
+
+        container.innerHTML += `
+        <div class="card">
+        <div class="img-box">
+            <span class="pin"><img src="./img/pin.svg" alt=""></span>
+            <img src="${url}" alt="">
+        </div>
+
+        <div class="caption">
+            <p class="data">${date}</p>
+            <h2 class="title">${title}</h2>
+        </div>
+        `;
+        
+    }
+    
 
 
 })
