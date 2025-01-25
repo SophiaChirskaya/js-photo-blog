1b2dc48f9d77673f0f3cc522b8527e66a89630e7
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-25 19:59:42 +0100

    [feat]consts

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Creo 
+// Seleziono gli elementi di output
+
+const containerCard = document.querySelector('.container');
+
+// Setto l'endpoint e i parametri relativi
+
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+
+// Lancio la richiesta AJAX verso l'API per ottenere l'ggetto di risposta
+
+axios.get(endpoint)
+.then(responseObj => {
+    // codice da eseguire in caso di successo
+    console.log(responseObj);
+
+
+})
