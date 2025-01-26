a0b39e4a1884714112550d9485e3a701beb363d8
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-26 15:38:48 +0100

    milestone 3

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 // Seleziono gli elementi di output
 
-const containerCard = document.querySelector('.container');
+const containerCard = document.getElementById('container');
 
-// Setto l'endpoint e i parametri relativi
+// // Setto l'endpoint e i parametri relativi
 
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
 
-// Lancio la richiesta AJAX verso l'API per ottenere l'ggetto di risposta
+// // Lancio la richiesta AJAX verso l'API per ottenere l'ggetto di risposta
 
 axios.get(endpoint)
 .then(responseObj => {
-    // codice da eseguire in caso di successo
+//     // codice da eseguire in caso di successo
     const pictures = responseObj.data;
     // console.log(pictures);
 
     for(let i = 0; i < pictures.length; i++){
         let picture = pictures[i];
         console.log(picture);
-        // Destrutturo l'oggetto
+//         // Destrutturo l'oggetto
         const {title, date, url} = picture;
 
         // Creo le card
 
         container.innerHTML += `
         <div class="card">
-        <div class="img-box">
-            <span class="pin"><img src="./img/pin.svg" alt=""></span>
-            <img src="${url}" alt="">
-        </div>
+            <div class="img-box">
+                <span class="pin"><img src="./img/pin.svg" alt=""></span>
+                <img src="${url}" alt="">
+            </div>
 
-        <div class="caption">
-            <p class="data">${date}</p>
-            <h2 class="title">${title}</h2>
+            <div class="caption">
+                <p class="data">${date}</p>
+                <h2 class="title">${title}</h2>
+            </div>
         </div>
         `;
         
@@ -40,3 +41,4 @@ axios.get(endpoint)
 
 
 })
+
